ef99863072a6eba2b4a88403acc4e9cda9491ee0
SFasel <sebastian.fasel@st.ovgu.de>
2019-10-09 09:54:39 +0200

    Erweiterung PowerShell-Demo
    GetArticleByNumberAndGroup

---
+++ Powershell-Demo/GetArticleByNumberAndGroup.ps1
@@ -0,0 +1,27 @@
+$baseurl = "http://demo.slmobile.de/demoapi/";
+
+# Login
+$accesstoken = Invoke-RestMethod -Uri "$baseurl/Login" -Method POST -ContentType 'application/json' -Body '{"username":"APIDemo", "password": "Ap1Dem0"}';
+
+$headerinfo = @{"Authorization"=$accesstoken.TokenType + " " + $accesstoken.AccessToken};
+
+$parameters =  "[{
+                    'Name':  'Artikelnummer',
+                    'Value':  '100001'
+                 },
+                 {
+                    'Name':  'Artikelgruppe',
+                    'Value':  '110'
+                 }]" 
+
+# Get Article
+$result = Invoke-RestMethod -Uri "$baseurl/Macros/SelectArticleByExplicitNumberAndGroup " -Method POST -ContentType 'application/json' -Body $parameters -Header $headerinfo
+
+$result
+
+$resultColumnNames = $result.ColumnNames | ConvertTo-Json
+$resultRows = $result.Rows | ConvertTo-Json
+
+$resultColumnNames 
+$resultRows
+
